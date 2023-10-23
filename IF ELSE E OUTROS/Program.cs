b2d5bda1aa146899c9eb3763eba14ad58e32c190
Willian Mariano <112572537+willianmarianosantos@users.noreply.github.com>
2023-10-23 00:55:02 -0300

    Add files via upload

---
+++ IF ELSE E OUTROS/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IF_ELSE_E_OUTROS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 10;
+            int b = 20;
+            int c = 200;
+
+            // SE MULHER && IDADE => 25 -> ENTRA NA FESTA DE GRAÇA
+
+
+            //se 
+            if (a < b || a > c)  { Console.WriteLine(" É verdade"); }
+
+
+            else if (a > c) { Console.WriteLine("Executou else if"); }
+
+
+            // se não
+            else  {  Console.WriteLine(" E MENTIRA "); }
+
+
+            Console.ReadLine();     
+
+
+
+
+
+
+        }
+    }
+}
---
+++ IF ELSE/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESTRUTURA_DE_ESCOLHA_IF_ELSE
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+
+            string nome = "";
+            Console.WriteLine("DIGITE O SEU NOME");
+            nome = Console.ReadLine();
+
+            if (nome == "willian")
+            {
+                Console.WriteLine("seu nome é");
+                Console.WriteLine(nome);
+            }
+            else
+            {
+                Console.WriteLine("CODIGO NAO ACEITO, VOLTE AO COMEÇO");
+            }
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
