92f1c557e09c5336c629ed654adcb2a1f39e78fe
Willian Mariano <112572537+willianmarianosantos@users.noreply.github.com>
2023-10-23 00:51:43 -0300

    Add files via upload

---
+++ matrix huehue/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matrix_huehue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] produtos = { 5, 10};
+
+            Console.WriteLine(produtos[0]);
+            Console.WriteLine(produtos[1]);
+
+
+            produtos[0] = 12;
+
+            Console.WriteLine(produtos[0]);
+            Console.WriteLine(produtos[1]);
+            Console.ReadLine();
+
+        }
+    }
+}
