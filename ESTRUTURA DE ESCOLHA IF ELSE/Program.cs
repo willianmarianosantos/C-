532c2758b40d9227a6f6d5531c256dc6ce73462b
Willian Mariano <112572537+willianmarianosantos@users.noreply.github.com>
2023-10-23 00:39:17 -0300

    Add files via upload

---
+++ ESTRUTURA DE ESCOLHA IF ELSE/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESTRUTURA_DE_ESCOLHA_IF_ELSE
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+
+
+
+            // OPERADORES LOGICOS 
+            // E/AND = &&  
+            // OU/OR = ||  
+
+
+
+            string nome = "";
+            Console.WriteLine("DIGITE O SEU NOME");
+            nome = Console.ReadLine();
+
+            if (nome == "willian") //SE
+            {
+                Console.WriteLine("seu nome é");
+                Console.WriteLine(nome);
+            }
+            else if ( nome == "seidao"    )  //
+            {
+                Console.WriteLine("quase");
+            }
+
+            else //SE NÃO
+            {
+                Console.WriteLine("CODIGO NAO ACEITO, VOLTE AO COMEÇO");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ enum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace @enum
+{
+    internal class Program
+    {
+        static void Main(string[] args){
+
+            int notas=0;
+
+
+
+
+
+
+            string resultadob = "aprovado";
+            string resultador = "reprovado";
+
+
+            if (notas > 10) { Console.WriteLine("A nota é  " + notas + " e esta " + resultadob); Console.ReadLine(); }
+            else { { Console.WriteLine("A nota é  " + notas + " e esta " + resultador); Console.ReadLine(); }}
+
+        }
+    }
+}
